428463f89ef3cf3ea355df3ef16a4e9f43e2f388
Dylan Garboski <dylgarbo@gmail.com>
2024-02-20 16:17:31 -0500

    fixed double scrollbar issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CardList from '../components/CardList'
-//import {robots} from '../components/robots'
 import SearchBox from '../components/SearchBox'
 import './App.css'
 import Scroll from '../components/Scroll'
